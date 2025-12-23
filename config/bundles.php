89412b9abb59afc6914fc2dd7af86517fe67eb85
Thierry R <thierry1804@gmail.com>
2025-12-23 23:23:34 +0100

    Correction: Vérifier l'existence de DebugBundle avant de l'enregistrer
    Évite l'erreur ClassNotFoundError en production où DebugBundle n'est pas installé (require-dev uniquement)

--- config/bundles.php
+++ config/bundles.php
@@ -1,8 +1,14 @@
 <?php
 
-return [
+$bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
+
+// Ajouter DebugBundle uniquement s'il est installé (en dev)
+if (class_exists(Symfony\Bundle\DebugBundle\DebugBundle::class)) {
+    $bundles[Symfony\Bundle\DebugBundle\DebugBundle::class] = ['dev' => true];
+}
+
+return $bundles;
